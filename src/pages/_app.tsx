e3b202463602926095e66080d12710327dea74c6
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-30 19:43:57 +0900

    formspree導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "../styles/tailwind.css";
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { FormspreeProvider } from "@formspree/react";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
+    <FormspreeProvider project="1580105528896388832">
       <Header />
       <Component {...pageProps} />
-    </>
+    </FormspreeProvider>
   );
 }
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function form(): JSX.Element {
+  const [state, handleSubmit] = useForm("contact");
+
   return (
     <>
       <div className="pt-28"></div>
       <div className=" h-auto w-2/3 my-10 mx-auto bg-white">
-        <form>
-          <input type="text" />
-        </form>
+        {state.succeeded ? (
+          <p>ご協力ありがとうございます!</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email Address</label>
+            <input id="email" type="email" name="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <textarea id="message" name="message" />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <button type="submit" disabled={state.submitting}>
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </>
   );
