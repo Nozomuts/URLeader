01921ca4343927601bd9e643b3cd2c3d2d96aeb9
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-03 23:47:39 +0900

    icon、アプリ名変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,61 +17,42 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja-JP" dir="ltr">
-        <title>UReserve</title>
+        <title>URLeader</title>
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
           <meta name="apple-mobile-web-app-title" content="UReserve" />
+          <link rel="apple-touch-icon" href="/icons/57x57.png" sizes="57x57" />
+          <link rel="apple-touch-icon" href="/icons/60x60.png" sizes="60x60" />
+          <link rel="apple-touch-icon" href="/icons/72x72.png" sizes="72x72" />
+          <link rel="apple-touch-icon" href="/icons/76x76.png" sizes="76x76" />
           <link
             rel="apple-touch-icon"
-            href="/icons/apple-icon-57x57.png"
-            sizes="57x57"
-          />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/apple-icon-60x60.png"
-            sizes="60x60"
-          />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/apple-icon-72x72.png"
-            sizes="72x72"
-          />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/apple-icon-76x76.png"
-            sizes="76x76"
-          />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/apple-icon-114x114.png"
+            href="/icons/114x114.png"
             sizes="114x114"
           />
           <link
             rel="apple-touch-icon"
-            href="/icons/apple-icon-120x120.png"
+            href="/icons/120x120.png"
             sizes="120x120"
           />
           <link
             rel="apple-touch-icon"
-            href="/icons/apple-icon-144x144.png"
+            href="/icons/144x144.png"
             sizes="144x144"
           />
           <link
             rel="apple-touch-icon"
-            href="/icons/apple-icon-152x152.png"
+            href="/icons/152x152.png"
             sizes="152x152"
           />
           <link
             rel="apple-touch-icon"
-            href="/icons/apple-icon-180x180.png"
+            href="/icons/180x180.png"
             sizes="180x180"
           />
-          <meta
-            name="msapplication-TileImage"
-            content="/icons/app-icon-144x144.png"
-          />
+          <meta name="msapplication-TileImage" content="/icons/144x144.png" />
           <meta name="msapplication-TileColor" content="#fff" />
           <meta name="theme-color" content="#282c34" />
           <meta
