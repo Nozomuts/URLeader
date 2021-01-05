8ef6645a4fe613472888b4ddebb455d77920a6de
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-05 19:56:40 +0900

    iconなど整理

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,53 +17,52 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja-JP" dir="ltr">
-        <title>URLeader</title>
         <Head>
-          {/** start_urlがlighthouseで検出されない https://github.com/shadowwalker/next-pwa/issues/107 */}
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
-            rel="stylesheet"
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
           />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-          <meta name="apple-mobile-web-app-title" content="URLeader" />
-          <link rel="apple-touch-icon" href="/icons/57x57.png" sizes="57x57" />
-          <link rel="apple-touch-icon" href="/icons/60x60.png" sizes="60x60" />
-          <link rel="apple-touch-icon" href="/icons/72x72.png" sizes="72x72" />
-          <link rel="apple-touch-icon" href="/icons/76x76.png" sizes="76x76" />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/114x114.png"
-            sizes="114x114"
+          <meta name="application-name" content="URLeader" />
+          <meta
+            name="msapplication-square70x70logo"
+            content="/icons/70x70.png"
           />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/120x120.png"
-            sizes="120x120"
+          <meta
+            name="msapplication-square150x150logo"
+            content="/icons/150x150.png"
           />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/144x144.png"
-            sizes="144x144"
+          <meta
+            name="msapplication-wide310x150logo"
+            content="/icons/310x150.png"
           />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/152x152.png"
-            sizes="152x152"
+          <meta
+            name="msapplication-square310x310logo"
+            content="/icons/310x310.png"
           />
-          <link
-            rel="apple-touch-icon"
-            href="/icons/180x180.png"
-            sizes="180x180"
-          />
-          <meta name="msapplication-TileImage" content="/icons/144x144.png" />
-          <meta name="msapplication-TileColor" content="#fff" />
+          <meta name="msapplication-TileColor" content="#282c34" />
           <meta name="theme-color" content="#282c34" />
           <meta
             name="description"
             content="URLと時間を登録して、予定を管理するアプリ"
+          />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
+          <link rel="icon" sizes="192x192" href="/icons/192x192.png" />
+          <link rel="icon" href="/icons/favicon.ico" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="#282c34"
+          />
+          <meta name="apple-mobile-web-app-title" content="URLeader" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/180x180.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
+            rel="stylesheet"
           />
           <script async defer src="https://apis.google.com/js/api.js"></script>
         </Head>
