a5ede0a2db038bff3115522d8b053f1eb4b7568c
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-07 21:13:53 +0900

    表示されるようにはなった

--- src/components/GAPI.tsx
+++ src/components/GAPI.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState, FC, Dispatch, SetStateAction } from "react";
+import { useState, FC, Dispatch, SetStateAction } from "react";
 import { SiGooglecalendar } from "react-icons/si";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import dayjs from "dayjs";
 import { ISchedule } from "../util/types";
 import { createSchedule } from "../db/schedule";
 import { Modal } from "./Modal";
+import Head from "next/head";
 
 type IProps = {
   setSchedule: Dispatch<SetStateAction<ISchedule[]>>;
@@ -15,82 +16,95 @@ export const GAPI: FC<IProps> = ({ setSchedule }) => {
   const [event, setEvent] = useState<Omit<ISchedule, "id">[]>([]);
   const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    const CLIENT_ID =
-      "840951746882-ksrhra0q5ikp5mt2se38aifbpmidmi2i.apps.googleusercontent.com";
-    const API_KEY = "AIzaSyDyDNlcwP4TRieJ7UUktzq2RB0EuGQ8V1s";
-    const DISCOVERY_DOCS = [
-      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-    ];
-    const SCOPES = "https://www.googleapis.com/auth/calendar.events";
+  const CLIENT_ID =
+    "840951746882-ksrhra0q5ikp5mt2se38aifbpmidmi2i.apps.googleusercontent.com";
+  const API_KEY = "AIzaSyDyDNlcwP4TRieJ7UUktzq2RB0EuGQ8V1s";
+  const DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ];
+  const SCOPES = "https://www.googleapis.com/auth/calendar.events";
+
+  function handleLoad() {
     const gapi = (window as any).gapi;
-    gapi.load("client:auth2", async () => {
-      await gapi.client.init({
+    gapi.load("client:auth2", initGAPI);
+  }
+
+  const initGAPI = () => {
+    const gapi = (window as any).gapi;
+    gapi.client
+      .init({
         apiKey: API_KEY,
         clientId: CLIENT_ID,
         discoveryDocs: DISCOVERY_DOCS,
         scope: SCOPES,
+      })
+      .then(() => {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(setAuth);
       });
-      setAuth(await gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
-  }, []);
-
-  const handleClick = () => {
-    const gapi = (window as any).gapi;
-    gapi.load("client:auth2", async () => {
-      if (!auth) {
-        await gapi.auth2.getAuthInstance().signIn();
-        setAuth(true);
-      }
-      const res = await gapi.client.calendar.events.list({
-        calendarId: "primary",
-        timeMin: new Date().toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        maxResults: 10,
-        orderBy: "startTime",
-      });
-      const arr: ISchedule[] = [];
-      const events = res.result.items;
-      events.forEach((event: any) => {
-        if ("conferenceData" in event || "hangoutLink" in event) {
-          arr.push({
-            id: dayjs().toString(),
-            memo: event.summary || "",
-            url:
-              event.hangoutLink ||
-              event.conferenceData.entryPoints[0].uri ||
-              "",
-            date:
-              dayjs(event.start.dateTime)
-                .format("YYYY/MM/DD H:mm")
-                .toString() || "",
-          });
-        }
-      });
-      setEvent(arr);
-      setOpen(true);
-    });
   };
 
-  function handleSignoutClick() {
+  const handleFetch = async () => {
+    const res = await (window as any).gapi.client.calendar.events.list({
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 10,
+      orderBy: "startTime",
+    });
+    const arr: ISchedule[] = [];
+    const events = res.result.items;
+    events.forEach((event: any) => {
+      if ("conferenceData" in event || "hangoutLink" in event) {
+        arr.push({
+          id: dayjs().toString(),
+          memo: event.summary || "",
+          url:
+            event.hangoutLink || event.conferenceData.entryPoints[0].uri || "",
+          date:
+            dayjs(event.start.dateTime).format("YYYY/MM/DD H:mm").toString() ||
+            "",
+        });
+      }
+    });
+    setEvent(arr);
+    setOpen(true);
+  };
+
+  const handleSignin = () => {
+    (window as any).gapi.auth2.getAuthInstance().signIn();
+  };
+
+  const handleSignout = () => {
     (window as any).gapi.auth2.getAuthInstance().signOut();
     setEvent([]);
     setAuth(false);
-  }
+  };
 
   return (
     <div>
+      <Head>
+        <script
+          async
+          defer
+          src="https://apis.google.com/js/api.js"
+          onLoad={handleLoad}
+        ></script>
+      </Head>
       {auth && (
         <button
           className="button mb-4"
-          onClick={handleSignoutClick}
+          onClick={handleSignout}
           aria-label="logout"
         >
           <RiLogoutBoxRLine size="20" className="text-red-500" />
         </button>
       )}
-      <button className="button mb-4" onClick={handleClick} aria-label="auth">
+      <button
+        className="button mb-4"
+        onClick={auth ? handleFetch : handleSignin}
+        aria-label="auth"
+      >
         <SiGooglecalendar size="20" className="text-blue-500" />
       </button>
       {open && (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -60,7 +60,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap"
             rel="stylesheet"
           />
-          <script async defer src="https://apis.google.com/js/api.js"></script>
         </Head>
         <body>
           <Main />
