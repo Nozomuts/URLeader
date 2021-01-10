403cafe7c9d39b092dc8b80c43b4d60b8d48611f
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-10 14:41:09 +0900

    electron戻す

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,11 +1,9 @@
-// Native
 import { join } from "path";
-// Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
+import { format } from "url";
+import { BrowserWindow, app, ipcMain, IpcMainEvent, shell } from "electron";
 import isDev from "electron-is-dev";
 import prepareNext from "electron-next";
 
-// Prepare the renderer once the app is ready
 app.on("ready", async () => {
   await prepareNext(".");
 
@@ -14,7 +12,6 @@ app.on("ready", async () => {
     height: 800,
     webPreferences: {
       nodeIntegration: false,
-      nativeWindowOpen: true,
       preload: join(__dirname, "preload.js"),
     },
   });
@@ -22,21 +19,22 @@ app.on("ready", async () => {
   // 外部ウィンドウで開く
   mainWindow.webContents.on("new-window", (event, url) => {
     event.preventDefault();
-    const win = new BrowserWindow({ show: false });
-    win.once("ready-to-show", () => win.show());
-    win.loadURL(url);
-    event.newGuest = win;
+    shell.openExternal(url);
   });
 
-  const url = isDev ? "http://localhost:8000/" : "https://urleader.vercel.app/";
+  const url = isDev
+    ? "http://localhost:8000/"
+    : format({
+        pathname: join(__dirname, "../out/index.html"),
+        protocol: "file:",
+        slashes: true,
+      });
 
   mainWindow.loadURL(url);
 });
 
-// Quit the app once all windows are closed
 app.on("window-all-closed", app.quit);
 
-// listen the channel `message` and resend the received message to the renderer process
 ipcMain.on("message", (event: IpcMainEvent, message: any) => {
   event.sender.send("message", message);
 });
