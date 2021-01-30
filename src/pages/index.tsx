b515c3925811906db586d2170716d4d30a7f35a3
NozomuTsuruta <tuno.0806@icloud.com>
2021-01-30 23:51:06 +0900

    refactor:push通知部分を切り出し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,17 @@
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { AddScheduleForm } from "../components/AddScheduleForm";
-import { readSchedule, deleteSchedule } from "../db/schedule";
 import { IFilter, ISchedule } from "../util/types";
 import { GAPI } from "../components/GAPI";
 import { menus } from "../util";
 import { Dropdown } from "../components/Dropdown";
 import { ScheduleList } from "../components/ScheduleList";
 import { SideMenu } from "../components/SideMenu";
+import useTimerNotif from "../util/useTimerNotif";
 
 export default function index() {
   const [filter, setFilter] = useState<IFilter>(menus[0]);
   const [schedule, setSchedule] = useState<ISchedule[]>([]);
-  const [timer, setTimer] = useState<NodeJS.Timeout[]>([]);
-
-  useEffect(() => {
-    if ("Notification" in window) {
-      // 通知の許可を求める
-      const permission = Notification.permission;
-      if (permission === "denied" || permission === "granted") {
-        return;
-      }
-      Notification.requestPermission().then(() => new Notification("テスト"));
-    }
-    // indexedDBから読み込む
-    readSchedule().then(setSchedule);
-  }, []);
-
-  useEffect(() => {
-    // scheduleが変更されたら初期化
-    timer.forEach((el) => clearTimeout(el));
-    schedule.forEach(({ url, date, id }) => {
-      const datetime = dayjs(date).valueOf();
-      const now = dayjs().valueOf();
-      const ONE_MIN = 60000;
-
-      // 1分前に通知
-      const setTimeBefore = datetime - now - ONE_MIN;
-      setTimer((prev) => [
-        ...prev,
-        setTimeout(() => {
-          if ("Notification" in window) {
-            const notif = new Notification("間も無く遷移します");
-            notif.addEventListener("show", () => {
-              new Audio("./push.wav").play();
-            });
-          }
-        }, setTimeBefore),
-      ]);
-
-      /** 予定時間と現在時刻の差 */
-      const setTime = datetime - now;
-      setTimer((prev) => [
-        ...prev,
-        setTimeout(() => {
-          if (window) {
-            window.open(url, "_blank");
-            deleteSchedule(id);
-            setSchedule((prev) => prev.filter((el) => el.id !== id));
-          }
-        }, setTime),
-      ]);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [schedule]);
+  useTimerNotif(schedule, setSchedule);
 
   /** 条件を満たす予定の数 */
   const scheduleLength = schedule.filter(({ date }) =>
