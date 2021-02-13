d614040e9119590fdf66bcf027c479bd1b21ab81
NozomuTsuruta <tuno.0806@icloud.com>
2021-02-13 20:01:50 +0900

    refactor: toastの処理をactionに追加

--- src/redux/records/actions.ts
+++ src/redux/records/actions.ts
@@ -1,40 +1,60 @@
 import { ActionTypes, IRecord } from "./types";
 import { recordsTable } from "../../db";
 import { Dispatch } from "redux";
+import { toast } from "react-toastify";
 
 export const createRecord = ({ ...record }: IRecord) => async (
   dispatch: Dispatch
 ) => {
-  await recordsTable.put({ ...record });
-  dispatch({
-    type: ActionTypes.CREATE_RECORD,
-    payload: record,
-  });
+  try {
+    await recordsTable.put({ ...record });
+    dispatch({
+      type: ActionTypes.CREATE_RECORD,
+      payload: record,
+    });
+    toast("追加しました");
+  } catch {
+    toast.error("追加に失敗しました");
+  }
 };
 
 export const readRecords = () => async (dispatch: Dispatch) => {
-  const records = await recordsTable.toArray();
-  dispatch({
-    type: ActionTypes.READ_RECORDS,
-    payload: records,
-  });
+  try {
+    const records = await recordsTable.toArray();
+    dispatch({
+      type: ActionTypes.READ_RECORDS,
+      payload: records,
+    });
+  } catch {
+    toast.error("データの読み込みに失敗しました");
+  }
 };
 
 export const updateRecord = (
   id: string,
   { ...record }: Partial<Omit<IRecord, "id">>
 ) => async (dispatch: Dispatch) => {
-  await recordsTable.update(id, record);
-  dispatch({
-    type: ActionTypes.UPDATE_RECORD,
-    payload: { id, ...record },
-  });
+  try {
+    await recordsTable.update(id, record);
+    dispatch({
+      type: ActionTypes.UPDATE_RECORD,
+      payload: { id, ...record },
+    });
+    toast("変更しました");
+  } catch {
+    toast.error("変更に失敗しました");
+  }
 };
 
 export const deleteRecord = (id: string) => async (dispatch: Dispatch) => {
-  await recordsTable.delete(id);
-  dispatch({
-    type: ActionTypes.DELETE_RECORD,
-    payload: id,
-  });
+  try {
+    await recordsTable.delete(id);
+    dispatch({
+      type: ActionTypes.DELETE_RECORD,
+      payload: id,
+    });
+    toast("削除しました");
+  } catch {
+    toast.error("削除に失敗しました");
+  }
 };
--- src/redux/schedule/actions.ts
+++ src/redux/schedule/actions.ts
@@ -2,41 +2,61 @@ import { ActionTypes, ISchedule } from "./types";
 import { Dispatch } from "redux";
 import { scheduleTable } from "../../db";
 import dayjs from "dayjs";
+import { toast } from "react-toastify";
 
 export const createSchedule = ({
   ...schedule
 }: Omit<ISchedule, "id">) => async (dispatch: Dispatch) => {
-  const id = dayjs().toString();
-  await scheduleTable.put({ id, ...schedule });
-  dispatch({
-    type: ActionTypes.CREATE_SCHEDULE,
-    payload: { id, ...schedule },
-  });
+  try {
+    const id = dayjs().toString();
+    await scheduleTable.put({ id, ...schedule });
+    dispatch({
+      type: ActionTypes.CREATE_SCHEDULE,
+      payload: { id, ...schedule },
+    });
+    toast("追加しました");
+  } catch {
+    toast.error("追加に失敗しました");
+  }
 };
 
 export const readSchedule = () => async (dispatch: Dispatch) => {
-  const schedule = await scheduleTable.toArray();
-  dispatch({
-    type: ActionTypes.READ_SCHEDULE,
-    payload: schedule,
-  });
+  try {
+    const schedule = await scheduleTable.toArray();
+    dispatch({
+      type: ActionTypes.READ_SCHEDULE,
+      payload: schedule,
+    });
+  } catch {
+    toast.error("データの読み込みに失敗しました");
+  }
 };
 
 export const updateSchedule = (
   id: string,
   { ...schedule }: Partial<Omit<ISchedule, "id">>
 ) => async (dispatch: Dispatch) => {
-  await scheduleTable.update(id, schedule);
-  dispatch({
-    type: ActionTypes.UPDATE_SCHEDULE,
-    payload: { id, ...schedule },
-  });
+  try {
+    await scheduleTable.update(id, schedule);
+    dispatch({
+      type: ActionTypes.UPDATE_SCHEDULE,
+      payload: { id, ...schedule },
+    });
+    toast("変更しました");
+  } catch {
+    toast.error("変更に失敗しました");
+  }
 };
 
 export const deleteSchedule = (id: string) => async (dispatch: Dispatch) => {
-  await scheduleTable.delete(id);
-  return dispatch({
-    type: ActionTypes.DELETE_SCHEDULE,
-    payload: id,
-  });
+  try {
+    await scheduleTable.delete(id);
+    dispatch({
+      type: ActionTypes.DELETE_SCHEDULE,
+      payload: id,
+    });
+    toast("削除しました");
+  } catch {
+    toast.error("削除に失敗しました");
+  }
 };
